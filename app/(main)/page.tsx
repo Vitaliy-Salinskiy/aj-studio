6c2cbdce413e6e2c1aaf417aa1e5a136df6fe7c5
Vitaliy-Salinskiy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-02-14 04:59:49 +0100

    Products implementation

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/shared/ProductCard";
 import { Slider } from "@/components/shared/Slider";
 import { getAllProducts } from "@/lib/requests";
 
@@ -7,6 +8,11 @@ export default async function Home() {
   return (
     <>
       <Slider />
+      <section className="flex justify-between gap-6 xl:gap-10 flex-wrap appContainer py-10 xl:py-16">
+        {[1, 2, 3, 4].map((_, i) => (
+          <ProductCard key={i} />
+        ))}
+      </section>
     </>
   );
 }
---
+++ components/shared/ProductCard.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+import { MdAddShoppingCart } from "react-icons/md";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const ProductCard = () => {
+  const [activeColor, setActiveColor] = useState();
+
+  return (
+    <div className="w-full sm:w-[calc(100%/2-24px)] lg:w-[calc(100%/3-24px)] xl:w-[calc(100%/4-40px)] flex flex-col gap-[32px]">
+      <div className="h-[282px] bg-own-lime rounded-3xl flex justify-center items-center">
+        <Image src="/shoe-1.png" alt="shoe-1" width={275} height={282} />
+      </div>
+      <div>
+        <h3>Nike air</h3>
+        <p className="text-own-light-red text-[19px] flex gap-[6px] mt-[8px]">
+          $200 <span className="line-through text-black">$300</span>
+        </p>
+        <div className="flex justify-between items-center mt-[13px]">
+          <Badge className="text-own-light-blue bg-own-opacity-blue hover:bg-own-opacity-blue text-[10px]">
+            Colors
+          </Badge>
+          <div className="flex gap-2">
+            {[1, 2, 3, 4, 5].map((_, i) => (
+              <div
+                key={i}
+                className="w-[11px] h-[11px] rounded-full bg-black border-double border-white border-2"
+              />
+            ))}
+          </div>
+        </div>
+        <Button className="w-full mt-[15px] bg-own-gray rounded-full flex gap-[11px] text-own-dark-blue hover:bg-own-gray">
+          <MdAddShoppingCart className="text-[#677585]" /> Add to cart
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
